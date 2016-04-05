594de044dffeb4e4d000e26732f3f1f3ec1d8acd
Yestin <ylh@pdx.edu>
2016-04-05 14:16:52 -0700

    initial implementation of OpenBSD Pledge

---
+++ asm.s
@@ -0,0 +1,6 @@
+// Everything below this line is ripped from the standard library source
+
+#include "textflag.h"
+
+TEXT ·use(SB),NOSPLIT,$0
+	RET
---
+++ openbsd_pledge.go
@@ -0,0 +1,28 @@
+// +build openbsd
+// +build 386,amd64,arm
+
+package extrasys
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+const (
+	SYS_PLEDGE = 108
+)
+
+func Pledge(promises string, paths []string) (err error) {
+	promisesp, err := syscall.BytePtrFromString(promises)
+	if err != nil {
+		return
+	}
+	pathsp, err := syscall.SlicePtrFromStrings(paths)
+	if err != nil {
+		return
+	}
+	_, _, e := syscall.Syscall(SYS_PLEDGE, uintptr(unsafe.Pointer(promisesp)), uintptr(unsafe.Pointer(pathsp)), 0)
+	use(unsafe.Pointer(promisesp))
+	use(unsafe.Pointer(&paths[0]))
+	return syscall.Errno(e)
+}
